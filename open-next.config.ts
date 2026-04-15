17774cea21f794663d34d444d8c11a3aeaa1d07f
jasiu7191a <jasiulen@gmail.com>
2026-04-15 18:34:24 +0200

    update

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,15 +1,15 @@
 import type { OpenNextConfig } from "@opennextjs/cloudflare";
-<<<<<<< HEAD
 
 const config: OpenNextConfig = {
   default: {
-    runtime: "edge",
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
   },
 };
 
-=======
-const config: OpenNextConfig = {
-  default: { runtime: "edge" },
-};
->>>>>>> 161f6b89d3c5e4d8c3c84a4c7eb6248713af7134
 export default config;
