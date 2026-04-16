ef7eb60a03c776b2de690b49619225b84ed0fc5b
jasiu7191a <jasiulen@gmail.com>
2026-04-16 15:21:48 +0200

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -5,14 +5,24 @@ const config: OpenNextConfig = {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
+      proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
       tagCache: "dummy",
       queue: "dummy",
     },
   },
-  // We remove the "external middleware" section to bundle it directly
-  // which is more reliable for Cloudflare Pages.
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
 };
 
 export default config;
-
