2e2a97d2c01f74eba7be345792c875ca18149065
jasiu7191a <jasiulen@gmail.com>
2026-04-18 22:40:38 +0200

    pushin p

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "rgba(255, 255, 255, 0.1)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
