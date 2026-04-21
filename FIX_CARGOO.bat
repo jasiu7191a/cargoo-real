011ce1450b3b408b76bf8303e0903a80d480e2cf
jasiu7191a <jasiulen@gmail.com>
2026-04-21 12:51:39 +0200

    pus

--- FIX_CARGOO.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "C:\Users\jasiu\OneDrive\Pulpit\cargoo-real-fixed"
-echo 🚀 Starting Mega-Fix...
-git add .
-git commit -m "Final Structural Synchronization"
-git push origin main
-echo ✅ Done! Your site is now updating.
-pause
